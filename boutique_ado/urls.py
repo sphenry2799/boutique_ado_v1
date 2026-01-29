c23863a412953a654f4f019aca08033811fcf6ab
sphenry2799 <sp.henry2799@gmail.com>
2026-01-29 15:19:50 +0000

    Add profile page

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -25,8 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
