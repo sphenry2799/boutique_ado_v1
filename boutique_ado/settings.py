60d98015656bcc245e87fda63ef72220827aa784
sphenry2799 <sp.henry2799@gmail.com>
2026-01-30 18:00:42 +0000

    Fix allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-sharon-66a29ca58045.herokuapp.com', 'localhost']
 
 # Application definition
 
